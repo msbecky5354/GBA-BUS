5fc9faeb773419a1246e5634c8b36a3e74ed16e0
msbecky5354 <msbgpt5354@gmail.com>
2026-07-18 18:26:30 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/GBA-BUS/', // <-- 必須加入呢行，名字要同你 GitHub Repo 完全一樣
 })
